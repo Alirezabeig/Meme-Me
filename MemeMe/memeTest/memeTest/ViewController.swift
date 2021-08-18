db68bac9fd4907eb8a9e5e3fbf06f31856634185
Alireza <Alirezabeig@gmail.com>
2021-08-17 21:54:54 -0700

    the image pickers and the relevant delegates are added

---
+++ MemeMe/memeTest/memeTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  memeTest
+//
+//  Created by Alireza on 8/17/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
