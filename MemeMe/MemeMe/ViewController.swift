236d036004f2058920f9dbb0dcf60cac9fa2e2e9
Alireza <Alirezabeig@gmail.com>
2021-08-17 22:18:16 -0700

    added camera files

--- MemeMe/MemeMe/ViewController.swift
+++ MemeMe/MemeMe/ViewController.swift
@@ -20,7 +20,6 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate,
     }
 
     @IBAction func pickAnImage(_ sender: UIBarButtonItem) {
-      
         pickerImage.allowsEditing = true
         pickerImage.delegate = self
         pickerImage.sourceType = .photoLibrary
@@ -29,7 +28,6 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate,
                }
                present(pickerImage, animated: true)
     }
-    
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
         if let image = info[.originalImage] as? UIImage {
             self.imagePreview.image = image
@@ -37,5 +35,15 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate,
 
         self.pickerImage.dismiss(animated: true, completion: nil)
     }
+    
+    @IBAction func camera(_ sender: UIBarButtonItem) {
+        
+        let imagePicker = UIImagePickerController()
+            imagePicker.delegate = self
+        imagePicker.sourceType = .camera
+            present(imagePicker, animated: true, completion: nil)
+            
+    }
+    
 }
 
